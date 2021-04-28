3f7b69540529e8649d22e6c67081d527e0583509
eniger31 <regina.garcia31@gmail.com>
2021-04-28 09:19:43 +0200

    END desktop style START mobile style

--- src/App.js
+++ src/App.js
@@ -5,6 +5,22 @@ function App() {
   return (
     <div className="App">
       <Nav />
+      <main className="main">
+        <h1> Accueil</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia, ratione?
+        </p>
+        <p> 
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A nemo sed neque, hic maiores veniam laudantium earum in aperiam magnam? 
+        </p>
+        <p> 
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A nemo sed neque, hic maiores veniam laudantium earum in aperiam magnam? 
+        </p>
+        <p> 
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A nemo sed neque, hic maiores veniam laudantium earum in aperiam magnam? 
+        </p>
+        
+     </main>
     </div>
   );
 }
